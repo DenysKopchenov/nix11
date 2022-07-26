c53f8a14e76e490b735ee37c508468bc527c6ce2
Denys Kopchenov <denisko555@gmail.com>
2022-07-26 13:55:47 +0300

    lesson 17
    -created parsers for xml and Json

--- src/main/java/com/alevel/lesson10/shop/Main.java
+++ src/main/java/com/alevel/lesson10/shop/Main.java
@@ -1,10 +1,110 @@
 package com.alevel.lesson10.shop;
 
-import com.alevel.lesson10.shop.controller.Controller;
+import com.alevel.lesson10.shop.model.phone.Manufacturer;
+import com.alevel.lesson10.shop.model.phone.OperatingSystem;
+import com.alevel.lesson10.shop.model.phone.Phone;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
-
     public static void main(String[] args) {
-        Controller.run();
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream inputStreamXML = classLoader.getResourceAsStream("phone.xml");
+        InputStream inputStreamJSON = classLoader.getResourceAsStream("phone.json");
+        List<String> lines = readAllLinesXML(inputStreamXML);
+        Map<String, String> fields = new HashMap<>();
+        parseLinesFromXMLToMap(lines, fields);
+        Phone phone = mapPhone(fields);
+        System.out.println("XML phone : " + phone);
+
+        lines = readAllLinesJSON(inputStreamJSON);
+        fields = new HashMap<>();
+
+        parseLinesFromJSONToMap(lines, fields);
+        Phone phoneJSON = mapPhone(fields);
+        System.out.println("JSON phone : " + phoneJSON);
+    }
+
+    private static Phone mapPhone(Map<String, String> map) {
+        OperatingSystem operatingSystem = new OperatingSystem();
+        operatingSystem.setDesignation(map.get("designation"));
+        operatingSystem.setVersion(Integer.parseInt(map.get("version")));
+        return new Phone(map.get("title"),
+                Integer.parseInt(map.get("count")),
+                Long.parseLong(map.get("price")),
+                map.get("model"),
+                Manufacturer.valueOf(map.get("manufacturer")),
+                LocalDateTime.parse(map.get("created"), DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")),
+                map.get("currency"),
+                operatingSystem);
+    }
+
+    private static void parseLinesFromXMLToMap(List<String> lines, Map<String, String> fields) {
+        lines.stream()
+                .map(String::trim)
+                .forEach(line -> {
+                    if (line.contains("<") && line.contains("</") && !line.startsWith("</")) {
+                        if (line.contains("currency")) {
+                            String substring = line.substring(line.indexOf('<') + 1, line.indexOf(" "));
+                            String price = line.substring(line.indexOf('>') + 1, line.indexOf("</"));
+                            fields.put(substring, price);
+                            String currency = line.substring(line.indexOf(" ") + 1, line.indexOf("="));
+                            String value = line.substring(line.indexOf("\"") + 1, line.lastIndexOf("\""));
+                            fields.put(currency, value);
+                        } else {
+                            String substring = line.substring(line.indexOf('<') + 1, line.indexOf('>'));
+                            String value = line.substring(line.indexOf('>') + 1, line.indexOf("</"));
+                            fields.put(substring, value);
+                        }
+                    }
+                });
+    }
+
+    private static List<String> readAllLinesXML(InputStream inputStream) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return lines;
+    }
+
+    private static void parseLinesFromJSONToMap(List<String> lines, Map<String, String> fields) {
+        Pattern pattern = Pattern.compile("\"\\w+\": \".*\"");
+        lines.stream()
+                .map(String::trim)
+                .forEach(line -> {
+                    Matcher matcher = pattern.matcher(line);
+                    if (matcher.find()) {
+                        String field = matcher.group().substring(1, line.indexOf(":") - 1);
+                        String value = matcher.group().substring(line.indexOf(":") + 3, line.lastIndexOf("\""));
+                        fields.put(field, value);
+                    }
+                });
+    }
+
+    private static List<String> readAllLinesJSON(InputStream inputStreamJSON) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStreamJSON))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return lines;
     }
 }
--- src/main/java/com/alevel/lesson10/shop/model/phone/Phone.java
+++ src/main/java/com/alevel/lesson10/shop/model/phone/Phone.java
@@ -2,6 +2,7 @@ package com.alevel.lesson10.shop.model.phone;
 
 import com.alevel.lesson10.shop.model.Product;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class Phone extends Product {
@@ -9,6 +10,10 @@ public class Phone extends Product {
     private String model;
     private Manufacturer manufacturer;
     private List<String> details;
+    private LocalDateTime creatingDate;
+    private String currency;
+    private OperatingSystem operatingSystem;
+
 
     public Phone(String title, int count, long price, String model, Manufacturer manufacturer) {
         super(title, count, price);
@@ -21,6 +26,15 @@ public class Phone extends Product {
         this.model = model;
         this.manufacturer = manufacturer;
         this.details = details;
+    }
+
+    public Phone(String title, int count, long price, String model, Manufacturer manufacturer, LocalDateTime creatingDate, String currency, OperatingSystem operatingSystem) {
+        super(title, count, price);
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.creatingDate = creatingDate;
+        this.currency = currency;
+        this.operatingSystem = operatingSystem;
     }
 
     public String getModel() {
@@ -47,11 +61,39 @@ public class Phone extends Product {
         this.details = details;
     }
 
+    public LocalDateTime getCreatingDate() {
+        return creatingDate;
+    }
+
+    public void setCreatingDate(LocalDateTime creatingDate) {
+        this.creatingDate = creatingDate;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public OperatingSystem getOperatingSystem() {
+        return operatingSystem;
+    }
+
+    public void setOperatingSystem(OperatingSystem operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("Phone{");
         sb.append("model='").append(model).append('\'');
         sb.append(", manufacturer=").append(manufacturer);
+        sb.append(", details=").append(details);
+        sb.append(", creatingDate=").append(creatingDate);
+        sb.append(", currency='").append(currency).append('\'');
+        sb.append(", operatingSystem=").append(operatingSystem);
         sb.append(", id='").append(id).append('\'');
         sb.append(", title='").append(title).append('\'');
         sb.append(", count=").append(count);
