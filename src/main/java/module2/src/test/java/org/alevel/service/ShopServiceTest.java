c9052a3a5a4e2d98f0101bb492f9235ceb38112b
Denys Kopchenov <denisko555@gmail.com>
2022-08-15 19:03:25 +0300

    Revert "Module 2 refactor"
    This reverts commit 09dc16c981d438840a20f53cc29eec6c2f0a8d22.

--- src/main/java/module2/src/test/java/org/alevel/service/ShopServiceTest.java
+++ src/main/java/module2/src/test/java/org/alevel/service/ShopServiceTest.java
@@ -15,6 +15,7 @@ import org.mockito.MockedStatic;
 import java.io.BufferedReader;
 import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -26,7 +27,7 @@ class ShopServiceTest {
 
     @BeforeEach
     void setUp() {
-        target = new ShopService();
+        target = new ShopService(new Random());
         reader = mock(BufferedReader.class);
     }
 
